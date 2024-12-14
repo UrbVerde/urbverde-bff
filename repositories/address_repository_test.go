568541ee758cead4ff8190d38d86b99bc04eedef
Felipe Corerato <felipe.corerato@ifood.com.br>
2024-12-14 09:31:17 -0300

    test: adds tests to address repository

---
+++ repositories/address_repository_test.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchAddress(t *testing.T) {
+	_ = godotenv.Load()
+
+	t.Run("Valid response", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`[
+				{"nome": "São Paulo", "microrregiao": {"mesorregiao": {"UF": {"sigla": "SP"}}}},
+				{"nome": "São Pedro", "microrregiao": {"mesorregiao": {"UF": {"sigla": "SP"}}}}
+			]`))
+		}))
+		defer server.Close()
+
+		os.Setenv("IBGE_API_URL", server.URL+"?nome=")
+
+		repo := NewExternalAddressRepository()
+		results, err := repo.SearchAddress("São")
+
+		assert.NoError(t, err)
+		assert.Equal(t, []string{"São Paulo - SP", "São Pedro - SP"}, results)
+	})
+}
