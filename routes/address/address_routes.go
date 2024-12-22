68301ada5296604419479084a534d6030066ef08
FelipeCorerato <fcorerato@gmail.com>
2024-12-22 20:19:41 -0300

    docs: adds swagger annotations to address route

--- routes/address/address_routes.go
+++ routes/address/address_routes.go
@@ -9,6 +9,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AddressSuggestion struct {
+	ID      int    `json:"id"`
+	Address string `json:"address"`
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
+// @Summary Retorna sugestões de endereço
+// @Description Retorna sugestões baseadas nos dados fornecidos
+// @Tags address
+// @Accept json
+// @Produce json
+// @Param query query string true "Texto para buscar sugestões"
+// @Success 200 {array} AddressSuggestion
+// @Failure 400 {object} ErrorResponse
+// @Router /address/suggestions [get]
 func SetupAddressRoutes(rg *gin.RouterGroup) {
 	// Inicializa o repositório e serviço
 	addressRepo := repositories.NewExternalAddressRepository()
