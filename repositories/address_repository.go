1c6201006b4f60cf07355b7c9006430d74fdba73
analiviagarbin <analiviamagalhaesg@gmail.com>
2024-12-27 13:26:50 -0300

    removing the query parameter from the request to ibge

--- repositories/address_repository.go
+++ repositories/address_repository.go
@@ -43,7 +43,7 @@ func NewExternalAddressRepository() AddressRepository {
 }
 
 func (r *externalAddressRepository) SearchAddress(query string) ([]string, error) {
-	url := r.apiURL + query
+	url := r.apiURL
 
 	resp, err := http.Get(url)
 	if err != nil {
